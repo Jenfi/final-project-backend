164e0f7c16fcb94c74685434cc8b62835690077d
Jenfi <jennifer_fick@hotmail.com>
2020-02-28 12:41:52 +0100

    set up user model

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose, { model } from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/haggle"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+// Check validation for email https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 40,
+    required: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Email is required']
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: [true, 'Password is required']
+  },
+  /* adds: {
+    type: []
+  }, */
+  /*   favourites: {
+      type: [],
+    }, */
+  /*   rating: {
+      type: Number,
+      default: 0
+    }, */
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+}
+
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
